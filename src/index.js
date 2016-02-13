b4552687576ef62e61d057319e7aae06d2c62942
Steven Silvestri <steven.m.silvestri@gmail.com>
2016-02-13 15:54:10 -0500

    removed FSA check, which was causing the promise to not resolve when more than 2 (type, payload) properties were passed from an action to a reducer.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import { isFSA } from 'flux-standard-action';
-
 function isPromise(val) {
   return val && typeof val.then === 'function';
 }
 
 export default function promiseMiddleware({ dispatch }) {
   return next => action => {
-    if (!isFSA(action)) {
-      return isPromise(action)
-        ? action.then(dispatch)
-        : next(action);
-    }
-
     return isPromise(action.payload)
       ? action.payload.then(
           result => dispatch({ ...action, payload: result }),
