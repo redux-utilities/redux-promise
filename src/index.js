e079f3f225b7309babc675cd6d5a0756a3ca3bf6
Jason Kurian <jasonk92@gmail.com>
2018-05-07 15:48:16 +0200

    chore: adds babel7 and package.scripts

--- src/index.js
+++ src/index.js
@@ -7,13 +7,12 @@ export default function promiseMiddleware({ dispatch }) {
     }
 
     return isPromise(action.payload)
-      ? action.payload.then(
-          result => dispatch({ ...action, payload: result }),
-          error => {
+      ? action.payload
+          .then(result => dispatch({ ...action, payload: result }))
+          .catch(error => {
             dispatch({ ...action, payload: error, error: true });
             return Promise.reject(error);
-          }
-        )
+          })
       : next(action);
   };
 }
@@ -21,7 +20,7 @@ export default function promiseMiddleware({ dispatch }) {
 // 'borrowed' from: https://github.com/then/is-promise/commit/ed0eaa4dec17597f0dae892a0472a9b7f459320d
 function isPromise(obj) {
   return (
-    !!obj &&
+    Boolean(obj) &&
     (typeof obj === 'object' || typeof obj === 'function') &&
     typeof obj.then === 'function'
   );
